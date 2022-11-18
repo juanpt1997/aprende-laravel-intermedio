eb8c7d732e543b1f5611db4820fc7ac25a0fbdf7
steven <gironsteven75@gmail.com>
2022-11-17 19:33:23 -0500

    creando las relaciones de caregory y projects desde codigo en cada modelo

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+     // Podemos pre cargar una consulta así:
+    // $category = Category::with('projects')->first();
+    // Esto me trae el project incluida los projects para evitar tantas consultas
+    // La próxima vez con $category->projects no realizará una consulta a la db
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -12,4 +12,13 @@ class Project extends Model
     {
         return 'url';
     }
+
+    // Podemos pre cargar una consulta así:
+    // $project = Project::with('category')->find(2);
+    // Esto me trae el project incluida la category para evitar tantas consultas
+    // La próxima vez con $project->category no realizará una consulta a la db
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
